d9db098fb184edd5880344170cf1574f8610d17b
Nimishms <83498563+Nimishms@users.noreply.github.com>
2021-07-10 15:29:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine,world;
+var backgroundImg
+var snowflake
+function preload() {
+  
+  backgroundImg = loadImage("snow2.jpg");
+}
+function setup() {
+  var canvas = createCanvas(1200,400);
+  engine = Engine.create();
+  world = engine.world;
+  snowflake = new Snowflakes(random(1,1100),50,50)
+}
+
+function draw() {
+  Engine.update(engine)
+  background(backgroundImg);  
+
+
+  for (var a = 0; a < snowflake.length; a++) {
+    snowflake[a].display();
+  }
+  
+}
